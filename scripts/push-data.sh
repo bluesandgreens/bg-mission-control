7d4842a43cd36f76e71ce638c15789e0aa10d9c2
Harry Quayle <bluesgreens@Harrys-Mac-mini.local>
2026-03-25 18:01:06 +1100

    data: auto-refresh 2026-03-25 18:00

--- scripts/push-data.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Push data files to GitHub after refresh
-cd /tmp/mc-live
-git add data/
-git commit -m "data: auto-refresh $(date '+%Y-%m-%d %H:%M')" --allow-empty
-git push origin main
